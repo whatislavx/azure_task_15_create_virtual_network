6f9754aa85091f650da86c6874ceb77a85d972be
Vladyslav Kozlov <kozlov.vlad@lll.kpi.ua>
2026-02-12 22:45:15 +0200

    Created virtual network with 3 subnets

--- task.ps1
+++ task.ps1
@@ -1,17 +1,39 @@
-$location = "uksouth"
+$location = "francecentral"
 $resourceGroupName = "mate-azure-task-15"
-
 $virtualNetworkName = "todoapp"
 $vnetAddressPrefix = "10.20.30.0/24"
 $webSubnetName = "webservers"
-$webSubnetIpRange = "???" # <- calculate subnet ip address range 
+$webSubnetIpRange = "10.20.30.0/26"
 $dbSubnetName = "database"
-$dbSubnetIpRange = "???" # <- calculate subnet ip address range 
+$dbSubnetIpRange = "10.20.30.64/26"
 $mngSubnetName = "management"
-$mngSubnetIpRange = "???" # <- calculate subnet ip address range 
+$mngSubnetIpRange = "10.20.30.128/26"
 
-Write-Host "Creating a resource group $resourceGroupName ..."
+Write-Host "Creating resource group..."
 New-AzResourceGroup -Name $resourceGroupName -Location $location
 
-Write-Host "Creating a virtual network ..."
-# write your code here -> 
+Write-Host "Creating virtual network..."
+$vnet = New-AzVirtualNetwork `
+    -Name $virtualNetworkName `
+    -ResourceGroupName $resourceGroupName `
+    -AddressPrefix $vnetAddressPrefix `
+    -Location $location
+
+Write-Host "Adding subnets..."
+
+$vnet = Add-AzVirtualNetworkSubnetConfig `
+    -Name $webSubnetName `
+    -AddressPrefix $webSubnetIpRange `
+    -VirtualNetwork $vnet
+
+$vnet = Add-AzVirtualNetworkSubnetConfig `
+    -Name $dbSubnetName `
+    -AddressPrefix $dbSubnetIpRange `
+    -VirtualNetwork $vnet
+
+$vnet = Add-AzVirtualNetworkSubnetConfig `
+    -Name $mngSubnetName `
+    -AddressPrefix $mngSubnetIpRange `
+    -VirtualNetwork $vnet
+
+Set-AzVirtualNetwork -VirtualNetwork $vnet
